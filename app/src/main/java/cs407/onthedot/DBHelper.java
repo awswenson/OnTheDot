86c7f03745df2825183ec4b4eb7129f89a584ee3
Alex Swenson <swensona@charter.net>
2016-04-03 12:50:42 -0500

    Modified the database class to handle adding/deleting/retrieving Trips

--- app/src/main/java/cs407/onthedot/DBHelper.java
+++ app/src/main/java/cs407/onthedot/DBHelper.java
@@ -1,140 +1,353 @@
 package cs407.onthedot;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by connerhuff on 3/9/16.
  */
 public class DBHelper extends SQLiteOpenHelper {
 
+    public static final String DATABASE_NAME = "OnTheDot.db";
+    public static final int DATABASE_VERSION = 1;
 
-    public static final String DATABASE_NAME = "MyDBName.db";
-    public static final String EVENTS_TABLE_NAME = "events";
-    public static final String EVENTS_COLUMN_ID = "id";
-    public static final String EVENTS_COLUMN_NAME = "name";
-    public static final String EVENTS_COLUMN_START = "start";
-    public static final String EVENTS_COLUMN_END = "end";
-    public static final String EVENTS_COLUMN_DATE = "date";
-    //public static final String CONTACTS_COLUMN_NAME = "name";
-    SQLiteDatabase db;
+    public static final String TRIP_TABLE_NAME = "TRIP";
+    public static final String TRIP_COLUMN_TRIP_ID = "TRIP_ID";
+    public static final String TRIP_COLUMN_DATE = "DATE";
+    public static final String TRIP_COLUMN_LATITUDE = "LATITUDE";
+    public static final String TRIP_COLUMN_LONGITUDE = "LONGITUDE";
+    public static final String TRIP_COLUMN_COMPLETE = "TRIP_COMPLETE";
 
-    public DBHelper(Context context)
-    {
-        super(context, DATABASE_NAME , null, 1);
+    public static final String PARTICIPANTS_TABLE_NAME = "PARTICIPANT";
+    public static final String PARTICIPANTS_COLUMN_TRIP_ID = "TRIP_ID";
+    public static final String PARTICIPANTS_COLUMN_PARTICIPANT_ID = "PARTICIPANT_ID";
+
+    /*
+      Use this date format when inserting or retrieving a date from the database.
+     */
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // TODO Auto-generated method stub
-        //db.openOrCreateDatabase("MyDBName.db", null, ((MainActivity) getActivity()).MODE_PRIVATE);
-        //db = this.getReadableDatabase();
         db.execSQL(
-                "create table events " +
-                        "(id integer primary key, name text,start text,end text, date text)"
+                "CREATE TABLE " + TRIP_TABLE_NAME + "(" +
+                        TRIP_COLUMN_TRIP_ID + " INTEGER PRIMARY KEY NOT NULL," +
+                        TRIP_COLUMN_DATE + " TEXT," +
+                        TRIP_COLUMN_LATITUDE + " REAL," +
+                        TRIP_COLUMN_LONGITUDE + " REAL," +
+                        TRIP_COLUMN_COMPLETE + " INTEGER)"
+        );
+
+        db.execSQL(
+                "CREATE TABLE " + PARTICIPANTS_TABLE_NAME + "(" +
+                        PARTICIPANTS_COLUMN_TRIP_ID + " INTEGER," +
+                        PARTICIPANTS_COLUMN_PARTICIPANT_ID + " TEXT)"
         );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // TODO Auto-generated method stub
-        db.execSQL("DROP TABLE IF EXISTS events");
+        db.execSQL("DROP TABLE IF EXISTS " + TRIP_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PARTICIPANTS_TABLE_NAME);
         onCreate(db);
     }
 
-    //insert a new tuple in the db
-    /*
-    public boolean insertEvent(String name, String start, String end, String date)
-    {
-        db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("start", start);
-        contentValues.put("end", end);
-        contentValues.put("date", date);
-        db.insert("events", null, contentValues);
-        return true;
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
     }
-    */
 
-    //gets a single tuple as identified by its ID
-    /*
-    public Cursor getData(int id){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from events where id="+id+"", null );
-        return res;
-    }
-    */
+    /**
+     * Adds the trip to the database.
+     *
+     * @param trip The Trip object that is to be added to the database
+     * @return Returns true if the trip was successfully added to the database; false otherwise
+     */
+    public boolean addTrip(Trip trip) {
+        boolean success = true;
 
-    //gets the number of rows in the database
-    /*
-    public int numberOfRows(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        int numRows = (int) DatabaseUtils.queryNumEntries(db, EVENTS_TABLE_NAME);
-        return numRows;
-    }
-    */
-
-    //example of a function that updates the values of a given entry in the database
-    /*
-    public boolean updateContact (Integer id, String name, String phone, String email, String street,String place)
-    {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("phone", phone);
-        contentValues.put("email", email);
-        contentValues.put("street", street);
-        contentValues.put("place", place);
-        db.update("events", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
-        return true;
+
+        db.beginTransaction();
+
+        try {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(TRIP_COLUMN_DATE, dateFormat.format(trip.getMeetupTime()));
+            contentValues.put(TRIP_COLUMN_LATITUDE, trip.getDestinationLatitude());
+            contentValues.put(TRIP_COLUMN_LONGITUDE, trip.getDestinationLongitude());
+            contentValues.put(TRIP_COLUMN_COMPLETE, ((trip.isTripComplete())? 1 : 0));
+
+            long tripID = db.insert(TRIP_TABLE_NAME, null, contentValues);
+
+            // Check to make sure the trip was successfully inserted into the database
+            if (tripID < 0) {
+
+                // The trip was not inserted successfully, so don't insert the participants
+                success = false;
+            }
+            else {
+
+                // Since the trip was inserted successfully, insert the participants
+                success = insertParticipants(tripID, trip.getFacebookFriendsIdList());
+            }
+
+            if (success) {
+                db.setTransactionSuccessful();
+            }
+        } catch (Exception e) { // Error in between database transaction
+            Log.d("DBHelper",
+                    "ERROR: Database Transaction was unsuccessful and threw an unexpected error",
+                    e.getCause());
+
+            success = false;
+        } finally { // Make sure to end the transaction
+            db.endTransaction();
+        }
+
+        return success;
     }
-    */
 
-    //delete from the database example
-    /*
-    public void deleteEvent (String name, String date) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("DELETE FROM events WHERE name = '" + name + "' AND date = '" + date + "';");
-    }
-    */
-
-    //example of a function that GETS information from the db
-    /*
-    public ArrayList<ScheduleEvent> getAllEventsFromDate(String date)
-    {
-        ArrayList<ScheduleEvent> array_list = new ArrayList<ScheduleEvent>();
-
-        //hp = new HashMap();
+    /**
+     * Gets the number of trips (rows) stored in the database.
+     *
+     * @return The number of trips stored in the database
+     */
+    public int numberOfTrips(){
         SQLiteDatabase db = this.getReadableDatabase();
-        //Cursor res =  db.rawQuery( "select * from events where 'date' = " + date, null );
-        String query = "select * from events where date = '" + date + "'";
-        //query = query.replace("/", "////");
-        Cursor res =  db.rawQuery(query, null );
+        return (int) DatabaseUtils.queryNumEntries(db, TRIP_TABLE_NAME);
+    }
+
+    /**
+     * Gets the trip in the database corresponding to the trip ID.
+     *
+     * @param tripID
+     * @return A Trip object corresponding to the trip ID
+     */
+    public Trip getTripByTripID(long tripID) {
+
+        Cursor res = getTripByIDWithParticipants(tripID);
+        res.moveToFirst();
+
+        // Gather all the trip information and create a new Trip object
+        LatLng destination = new LatLng(res.getDouble(res.getColumnIndex(TRIP_COLUMN_LATITUDE)),
+                res.getDouble(res.getColumnIndex(TRIP_COLUMN_LATITUDE)));
+
+        boolean tripComplete = (res.getInt(res.getColumnIndex(TRIP_COLUMN_COMPLETE)) == 1);
+
+        Date meetupTime;
+
+        try {
+            meetupTime = dateFormat.parse(res.getString(res.getColumnIndex(TRIP_COLUMN_DATE)));
+        } catch (ParseException e) {
+            Log.d("DBHelper", "ERROR: Corrupted trip (ID: " +
+                    tripID + ") due to unparsable date stored in database");
+
+            return null;
+        }
+
+        Trip trip = new Trip(tripID, destination, meetupTime, new ArrayList<String>(), tripComplete);
+
+        // Get all the participant IDs and add them to the FacebookFriendsIdList in the trip object
+        while(!res.isAfterLast()) {
+            trip.getFacebookFriendsIdList().add(
+                    res.getString(res.getColumnIndex(PARTICIPANTS_COLUMN_PARTICIPANT_ID)));
+
+            res.moveToNext();
+        }
+
+        return trip;
+    }
+
+    /**
+     * Get all the Trips currently stored in the database.
+     *
+     * @return An ArrayList containing all the Trip objects stored in the database
+     */
+    public ArrayList<Trip> getAllTrips() {
+        ArrayList<Trip> trips = new ArrayList<>();
+
+        Cursor res = getAllTripIDs();
 
         res.moveToFirst();
 
-        while(res.isAfterLast() == false){
+        while(!res.isAfterLast()) {
+            Trip trip = getTripByTripID(res.getLong(res.getColumnIndex(TRIP_COLUMN_TRIP_ID)));
 
-            String name = res.getString(res.getColumnIndex(EVENTS_COLUMN_NAME));
-            String start = res.getString(res.getColumnIndex(EVENTS_COLUMN_START));
-            String end = res.getString(res.getColumnIndex(EVENTS_COLUMN_END));
-            String currDate = res.getString(res.getColumnIndex(EVENTS_COLUMN_DATE));
+            if (trip != null) {
+                trips.add(trip);
+            }
 
-            ScheduleEvent s = new ScheduleEvent(name, start, end, currDate);
-            array_list.add(s);
             res.moveToNext();
         }
-        return array_list;
-    }
-    */
 
-    //clear all tables. Use for debugging purposes.
-    /*
-    public void restartDBDebug(){
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("DROP TABLE IF EXISTS events");
+        return trips;
     }
-    */
+
+    /**
+     * Delete a trip from the database corresponding to the trip ID
+     *
+     * @param tripID The trip ID corresponding to the trip to delete
+     */
+    public void deleteTripByTripID(long tripID) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("DELETE FROM");
+        sb.append(TRIP_TABLE_NAME);
+        sb.append(" WHERE ");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(" = ");
+        sb.append(tripID);
+
+        // Delete the trip corresponding to the trip ID
+        db.execSQL(sb.toString());
+
+        sb = new StringBuilder();
+
+        sb.append("DELETE FROM");
+        sb.append(PARTICIPANTS_TABLE_NAME);
+        sb.append(" WHERE ");
+        sb.append(PARTICIPANTS_COLUMN_TRIP_ID);
+        sb.append(" = ");
+        sb.append(tripID);
+
+        // Delete the trip's participants corresponding trip ID
+        db.execSQL(sb.toString());
+    }
+
+    private boolean insertParticipants(long tripID, List<String> facebookFriendsIdList) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        for (String participant : facebookFriendsIdList) {
+            ContentValues contentValues = new ContentValues();
+
+            contentValues.put(PARTICIPANTS_COLUMN_TRIP_ID, tripID);
+            contentValues.put(PARTICIPANTS_COLUMN_PARTICIPANT_ID, participant);
+
+            if (db.insert(PARTICIPANTS_TABLE_NAME, null, contentValues) < 0) {
+              return false;
+            }
+        }
+
+        return true;
+    }
+
+    private Cursor getTripByIDWithParticipants(long tripID){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT ");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(", ");
+        sb.append(TRIP_COLUMN_DATE);
+        sb.append(", ");
+        sb.append(TRIP_COLUMN_LATITUDE);
+        sb.append(", ");
+        sb.append(TRIP_COLUMN_LONGITUDE);
+        sb.append(", ");
+        sb.append(TRIP_COLUMN_COMPLETE);
+        sb.append(", ");
+        sb.append(PARTICIPANTS_COLUMN_PARTICIPANT_ID);
+        sb.append(" FROM ");
+        sb.append(TRIP_TABLE_NAME);
+        sb.append(" t WHERE ");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(" = ");
+        sb.append(tripID);
+        sb.append(" INNER JOIN ");
+        sb.append(PARTICIPANTS_TABLE_NAME);
+        sb.append(" p ON t.");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(" = p.");
+        sb.append(PARTICIPANTS_COLUMN_TRIP_ID);
+
+        return db.rawQuery(sb.toString(), null);
+    }
+
+    private Cursor getTripByIDWithoutParticipants(long tripID){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT * FROM ");
+        sb.append(TRIP_TABLE_NAME);
+        sb.append(" WHERE ");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(" = ");
+        sb.append(tripID);
+
+        return db.rawQuery(sb.toString(), null);
+    }
+
+    private Cursor getParticipantsByTripID(long tripID){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT ");
+        sb.append(PARTICIPANTS_COLUMN_PARTICIPANT_ID);
+        sb.append(" FROM ");
+        sb.append(PARTICIPANTS_TABLE_NAME);
+        sb.append(" WHERE ");
+        sb.append(PARTICIPANTS_COLUMN_TRIP_ID);
+        sb.append(" = ");
+        sb.append(tripID);
+
+        return db.rawQuery(sb.toString(), null);
+    }
+
+    private Cursor getAllTripIDs(){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT ");
+        sb.append(TRIP_COLUMN_TRIP_ID);
+        sb.append(" FROM ");
+        sb.append(TRIP_TABLE_NAME);
+
+        return db.rawQuery(sb.toString(), null);
+    }
+
+//    //example of a function that updates the values of a given entry in the database
+//    public boolean updateContact (Integer id, String name, String phone, String email, String street,String place)
+//    {
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put("name", name);
+//        contentValues.put("phone", phone);
+//        contentValues.put("email", email);
+//        contentValues.put("street", street);
+//        contentValues.put("place", place);
+//        db.update("events", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+//        return true;
+//    }
+//
+//    //clear all tables. Use for debugging purposes.
+//    public void restartDBDebug(){
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        db.execSQL("DROP TABLE IF EXISTS events");
+//    }
 }
--- app/src/main/java/cs407/onthedot/Trip.java
+++ app/src/main/java/cs407/onthedot/Trip.java
@@ -2,10 +2,8 @@ package cs407.onthedot;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by Alex Swenson on 4/2/16.
@@ -13,18 +11,20 @@ import java.util.Locale;
 public class Trip {
 
     /*
+      This is the ID that is assigned by the database to refer to this trip object.  DO NOT
+      explicitly set the tripID expect for with the tripID that is returned by the database.
+     */
+    private long tripID;
+
+    /*
       Stores the latitude/longitude of the meet-up destination.  We can use this to
       pinpoint the location on a Google maps widget, and use it to determine a person's
-      distance from the destination.  When we store the destination in the database, we
-      will need to store the latitude/longitude separately as two Strings by getting their
-      double representations and converting to a String.
+      distance from the destination.
      */
     private LatLng destination;
 
     /*
-      Stored in a Date object, this stores the date and time of the meet-up.  When we store
-      it in the database, however, we will need to convert the date into a String with the
-      format "yyyy-MM-dd HH:mm:ss."  Use the getMeetupTimeAsString() method to accomplish this.
+      Stored in a Date object, this stores the date and time of the meet-up.
      */
     private Date meetupTime;
 
@@ -38,10 +38,38 @@ public class Trip {
      */
     private List<String> facebookFriendsIdList;
 
-    public Trip(LatLng destination, Date meetupTime, List<String> facebookFriendsIdList) {
+    /*
+      A boolean variable to identify if the trip is complete (true) or is in
+      progress/upcoming (false).
+     */
+    private boolean tripComplete;
+
+    public Trip(LatLng destination, Date meetupTime, List<String> facebookFriendsIdList,
+                boolean tripComplete) {
         this.destination = destination;
         this.meetupTime = meetupTime;
         this.facebookFriendsIdList = facebookFriendsIdList;
+        this.tripComplete = tripComplete;
+    }
+
+    /*
+      Only use this when the tripID is known (i.e. been assigned by the database)
+     */
+    public Trip(long tripID, LatLng destination, Date meetupTime, List<String> facebookFriendsIdList,
+                boolean tripComplete) {
+        this.tripID = tripID;
+        this.destination = destination;
+        this.meetupTime = meetupTime;
+        this.facebookFriendsIdList = facebookFriendsIdList;
+        this.tripComplete = tripComplete;
+    }
+
+    public long getTripID() {
+        return tripID;
+    }
+
+    public void setTripID(long tripID) {
+        this.tripID = tripID;
     }
 
     public LatLng getDestination() {
@@ -64,13 +92,6 @@ public class Trip {
         return meetupTime;
     }
 
-    public String getMeetupTimeAsString() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-
-        return dateFormat.format(meetupTime);
-    }
-
     public void setMeetupTime(Date meetupTime) {
         this.meetupTime = meetupTime;
     }
@@ -81,5 +102,13 @@ public class Trip {
 
     public void setFacebookFriendsIdList(List<String> facebookFriendsIdList) {
         this.facebookFriendsIdList = facebookFriendsIdList;
+    }
+
+    public boolean isTripComplete() {
+        return tripComplete;
+    }
+
+    public void setTripComplete(boolean tripComplete) {
+        this.tripComplete = tripComplete;
     }
 }
