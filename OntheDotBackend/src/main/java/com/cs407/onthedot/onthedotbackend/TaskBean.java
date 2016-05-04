c9566722f7cd5357fd983d76c22e853464c6e9bc
chuff2 <chuff2@wisc.edu>
2016-05-03 19:18:19 -0500

    added something to test out new branch

--- OntheDotBackend/src/main/java/com/cs407/onthedot/onthedotbackend/TaskBean.java
+++ OntheDotBackend/src/main/java/com/cs407/onthedot/onthedotbackend/TaskBean.java
@@ -9,6 +9,16 @@ import com.googlecode.objectify.annotation.Id;
 public class TaskBean {
     @Id
     private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
